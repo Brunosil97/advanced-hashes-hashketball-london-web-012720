d64742c008e5426fc9fb5e76e692a224175ca27b
Bruno Silva <brunosilvinosilva@gmail.com>
2020-01-27 11:15:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,12 +126,13 @@ def num_points_scored(players_name)
       data.each do |player|
         if player[:player_name] == players_name
           return player[:points]
-          binding.pry
+          
         end 
       end 
      end 
     end 
   end
+  binding.pry
 end 
 
 def shoe_size(players_name)
